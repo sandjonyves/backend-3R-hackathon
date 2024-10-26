cf792c89e5643267daf0dae33e9401be37720d05
sandjonyves <sandjonyves@gmail.com>
2024-10-26 07:21:19 +0000

    postgrest-add

--- Backend/settings.py
+++ Backend/settings.py
@@ -165,8 +165,12 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILS_DIRS =[BASE_DIR/'static/']
+STATICFILES_DIRS =[BASE_DIR/'static/']
 
+
+# STATICFILES_DIRS = [ 
+#      os.path.join(BASE_DIR, 'static'), 
+#  ] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
